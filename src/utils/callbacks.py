c51f284344872041f690ffffd2a60b843f042a56
Srinivas Rahul Sapireddy <sapireddyrahul@gmail.com>
2022-01-22 17:01:44 -0600

    joblib requirements added

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -1,7 +1,20 @@
-
+import tensorflow as tf
+import os
+import time 
+import joblib # to store this information as binary
+import logging
+from src.utils.all_utils import get_timestamp
 
 def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
-    pass
+    unique_name = get_timestamp("tb_logs")
+
+    tb_running_log_dir = os.path.join(tensorboard_log_dir, unique_name)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
+
+    tb_callback_filepath = os.path.join(callbacks_dir, "tensorboard_cb.cb")
+    joblib.dump(tensorboard_callback, tb_callback_filepath)
+
+    logging.info(f"Tensorboard callback is being saved at {tb_callback_filepath}")
 
 def create_and_save_checkpoint_Callback(callbacks_dir, checkpoint_dir):
     pass
